6c27b529be921c1eb957cff852cee4fac66ce2e2
Thural M <tural.musaibov@outlook.com>
2023-05-03 00:46:22 +0400

    add JsonServer

---
+++ JsonServer/db.js
@@ -0,0 +1,9 @@
+[
+    {
+        jobTitle: "Satış məsləhətçisi (Elektronika –Masallı, Beyləqan)",
+        companyName: "ELIT.TO",
+        date: "02 May",
+        descripton: `“Elit Optimal “ -  is a subsidiary of “Elit Electronics”, the largest electronics retailer in the Georgian market. “Elit Electronics” is a subsidiary of “Brimag Digital Age”, that is operated  in Israeli market and listed at Tel Aviv Stock Exchange. `,
+
+    }
+]
